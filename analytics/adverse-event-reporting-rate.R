c776e8f84c2de9c1951a93b6f48f13b0a06856c9
Spencer Childress <spencer.c.childress@gmail.com>
2025-02-14 09:25:40 -0500

    adapt workflows to sdtm data

---
+++ analytics/adverse-event-reporting-rate.R
@@ -0,0 +1,74 @@
+library(purrr)
+library(dplyr)
+library(gsm)
+
+# 0. Input data
+# ----
+
+lRawData <- list(
+    dm = readRDS('data/dm.rds'),
+    ae = readRDS('data/ae.rds')
+)
+
+# 1. Mapped data
+# ----
+
+lMappingWorkflows <- gsm::MakeWorkflowList(
+    strPath = 'workflows/1_mappings',
+    strPackage = NULL
+)
+
+lMappedData <- gsm::RunWorkflows(
+    lMappingWorkflows,
+    lRawData
+)
+
+# 2. Analysis Data
+# ----
+
+lAnalysisWorkflows <- gsm::MakeWorkflowList(
+    strPath = 'workflows/2_metrics',
+    strPackage = NULL
+)
+
+lAnalysisData <- gsm::RunWorkflows(
+    lAnalysisWorkflows,
+    lMappedData
+)
+
+# 3. Reporting data
+# ----
+
+lReportingWorkflows <- gsm::MakeWorkflowList(
+    strPath = 'workflows/3_reporting',
+    strPackage = NULL
+)
+
+lReportingData <- gsm::RunWorkflows(
+    lReportingWorkflows,
+    list(
+        lAnalyzed = lAnalysisData,
+        lWorkflows = lAnalysisWorkflows,
+        mapped_site = lMappedData$mapped_site
+    )
+)
+
+# 4. Output
+# ----
+
+lModuleWorkflows <- gsm::MakeWorkflowList(
+    strPath = 'workflows/4_modules',
+    strPackage = NULL
+)
+
+# TODO: figure out why this is breaking
+RunWorkflows(
+    lModuleWorkflows,
+    lReportingData
+)
+Widget_ScatterPlot(
+    lReportingData$Reporting_Results,
+    as.list(lReportingData$Reporting_Metrics),
+    lReportingData$Reporting_Groups,
+    lReportingData$Reporting_Bounds
+)
